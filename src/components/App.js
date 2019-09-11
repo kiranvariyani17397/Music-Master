93f23cd1918970b369a76d5084cc0205d69b805c
kiranvariyani17397 <kiranvariyani1731997@gmail.com>
2019-09-11 20:15:53 +0530

    artist changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Artist from "./Artist";
+import Tracks from "./Tracks";
 
 const API_ADDRESS = "https://spotify-api-wrapper.appspot.com";
 
@@ -21,18 +22,20 @@ class App extends Component {
       return response
         .json()
         .then(json => {
-          const artist = json.artists.items[0];
-          this.setState({ artist });
-          fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`).then(
-            response => {
-              return response
-                .json()
-                .then(json => {
-                  this.setState({ tracks: json.tracks });
-                })
-                .catch(error => alert(error.message));
-            }
-          );
+          if(json.artists.total > 0) {
+            const artist = json.artists.items[0];
+            this.setState({ artist });
+            fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`).then(
+              response => {
+                return response
+                  .json()
+                  .then(json => {
+                    this.setState({ tracks: json.tracks });
+                  })
+                  .catch(error => alert(error.message));
+              }
+            );
+          }
         })
         .catch(error => alert(error.message));
     });
@@ -48,7 +51,12 @@ class App extends Component {
           placeholder="Search for an Artist"
         />
         <button onClick={this.searchArtist}>Search</button>
+        {this.state.artist ? (
         <Artist artist={this.state.artist} />
+        ) : null}
+        {this.state.tracks.length > 0 ? (
+          <Tracks tracks={this.state.tracks} />
+        ): null}
       </div>
     );
   }
--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,11 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Artist extends Component {
-  render() {
-    console.log("artist", this.props.artist);
-
-    return <div>Artist</div>;
+const Artist = ({artist}) => {
+    const {name, followers, genres, images} = artist
+    return (
+    <div>
+      <h3>{name}</h3>
+      <p>{followers.total} followers</p>
+      <p>{genres.join(",")}</p>
+      <img src={images[0] && images[0].url}
+       alt="artist-profile"
+       style={{
+         width: 200,
+         height: 200,
+         borderRadius: 100,
+         ObjectFit: 'cover'
+       }}
+      />
+    </div>
+    );
   }
-}
 
 export default Artist;
